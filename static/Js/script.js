5304975aa98f95a82819785d1035338bfefb2cb6
shalom <oluwafemishalom3@gmail.com>
2023-08-10 15:22:47 +0100

    make new changes

--- static/Js/script.js
+++ static/Js/script.js
@@ -1,40 +1,82 @@
-let navEl = document.querySelector('.navbar')
+let navEl = document.querySelector(".navbar");
 
-window.addEventListener('scroll',()=>{
-    if(window.scrollY > 50){
-navEl.classList.add('nav-scrolled')
-    }
-    else if(window.scrollY<150){
-        navEl.classList.remove('nav-scrolled')
-    }
-})
-// Add an event listener to the "Learn More" button
-const learnMoreButton = document.querySelector('.learn-div');
-const modalTitle = document.querySelector('.modal-title');
-const modalImage = document.querySelector('.modal-image');
-const modalBody = document.querySelector('.modal-body');
-
-learnMoreButton.addEventListener('click', () => {
-  fetch('json\plant_category.json') 
-    .then(response => response.json())
-    .then(data => {
-      // Populate modal content with data from the JSON
-      modalTitle.textContent = data.name;
-      modalImage.src = data.image; // Set the image source
-      modalImage.alt = data.name; // Set the alt attribute for accessibility
-
-      modalBody.innerHTML = `
-        <p><strong>Category</strong>: ${data.category}</p>
-        <p><strong>Botanical Name</strong>: ${data.botanicalName}</p>
-        <p><strong>Price</strong>: ${data.price}</p>
-        <p><strong>Light</strong>: ${data.details.light}</p>
-        <p><strong>Water</strong>: ${data.details.water}</p>
-        <p><strong>Size</strong>: ${data.details.size}</p>
-        <p><strong>Blooms</strong>: ${data.details.blooms}</p>
-        
-      `;
-    })
-    .catch(error => {
-      console.error('Error fetching JSON data:', error);
-    });
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navEl.classList.add("nav-scrolled");
+  } else if (window.scrollY < 150) {
+    navEl.classList.remove("nav-scrolled");
+  }
 });
+
+// Search
+const searchInput = document.querySelector('input[type="search"]');
+const boxes = document.querySelectorAll(".box");
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase().trim();
+
+  boxes.forEach((box) => {
+    const title = box
+      .querySelector(".boxed-bottom-text p")
+      .textContent.toLowerCase();
+
+    if (title.includes(searchTerm)) {
+      box.style.display = "block";
+    } else {
+      box.style.display = "none";
+    }
+  });
+});
+
+// Json
+
+let storeData = [];
+
+window.onload = () => {
+  fetch("/json/plant_category.json")
+    .then((response) => response.json())
+    .then((res) => {
+      storeData = res.data;
+      console.log("store data", storeData);
+
+      loadCards()
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON data:", error);
+    });
+
+    
+  };
+ 
+
+const learnMoreButton = document.querySelector(".learn-div");
+const modalTitle = document.querySelector(".modal-title");
+const modalImage = document.querySelector(".modal-image");
+const modalBody = document.querySelector(".modal-body");
+
+
+const boxesWrapper = document.querySelector(".boxs-wrapper");
+
+
+
+
+
+function openStoreModal(id) {
+  modalTitle.textContent = storeData[id].name;
+  modalImage.src = storeData[id].image;
+  modalImage.alt = storeData[id].name;
+
+  modalBody.innerHTML = `
+        <p><strong>Category</strong>: ${storeData[id]?.category}</p>
+        <p><strong>Botanical Name</strong>: ${storeData[id]?.botanicalName}</p>
+        <p><strong>Price</strong>: ${storeData[id]?.price}</p>
+        <p><strong>Light</strong>: ${storeData[id].details.light}</p>
+        <p><strong>Water</strong>: ${storeData[id].details.water}</p>
+        <p><strong>Size</strong>: ${storeData[id].details.size}</p>
+        <p><strong>Blooms</strong>: ${storeData[id].details.blooms}</p>
+      `;
+}
+
+openStoreModal(0);
+
+
